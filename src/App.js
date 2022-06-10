2ada6e7413f693ff523c8d60a484ca5f947fc017
sarazaToussaint <tmsaraza@gmail.com>
2022-06-10 13:48:04 +0200

    Setup linters in the project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Book from './components/Book';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Book />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Book.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Book = () => (
+  <div>
+    <h1>Hello from React</h1>
+    <button type="submit">Click me</button>
+  </div>
+);
+
+export default Book;
