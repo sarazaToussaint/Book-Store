46a6f17dd8e14bfae0123a100c89d0725085ae1d
sarazaToussaint <tmsaraza@gmail.com>
2022-06-16 18:54:00 +0200

    Impliment the post request action

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,22 +2,28 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { booksActions } from '../redux/books/books';
+import { deleteBookData } from '../redux/books/actions';
 import classes from './Book.module.css';
 
-const Book = ({ id, title, author }) => {
+const Book = ({
+  id, title, author, category,
+}) => {
   const dispatch = useDispatch();
 
   const removeHandler = (e) => {
+    dispatch(deleteBookData(e.target.id));
     dispatch(booksActions.removeBook(e.target.id));
   };
-
   return (
     <div className={classes.book}>
+      <div className={classes.category}>{category}</div>
       <h3 className="title">{title}</h3>
       <div className="author">{author}</div>
       <div className={classes.buttons}>
         <button type="button">Comments</button>
-        <button id={id} type="button" onClick={removeHandler}>Remove</button>
+        <button id={id} type="button" onClick={removeHandler}>
+          Remove
+        </button>
         <button type="button">Edit</button>
       </div>
     </div>
@@ -25,9 +31,10 @@ const Book = ({ id, title, author }) => {
 };
 
 Book.propTypes = {
-  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
 };
 
 export default Book;
