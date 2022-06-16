839286943b21c1c386a17658570fead718239d02
sarazaToussaint <tmsaraza@gmail.com>
2022-06-16 17:59:45 +0200

    Delete unused codes

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -23,30 +23,3 @@ const books = createSlice({
 export const booksActions = books.actions;
 
 export default books;
-
-/* eslint-disable no-param-reassign */
-// import { createSlice } from '@reduxjs/toolkit';
-
-// const initialState = {
-//   books: [],
-// };
-
-// const books = createSlice({
-
-//   name: 'book',
-//   initialState,
-
-//   reducers: {
-//     addBook(state, action) {
-//       state.books.push(action.payload);
-//     },
-
-//     removeBook(state, action) {
-//       state.books = state.books.filter((book) => book.id !== action.payload);
-//     },
-//   },
-// });
-
-// export const booksActions = books.actions;
-
-// export default books;
